6b9aba3be83c78cde7f47c98b17848e987a8c044
LabRatForms <141075494+LabRatForms@users.noreply.github.com>
2023-07-31 17:07:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,31 @@
 import streamlit_authenticator as stauth
 
+hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
+
+import yaml
+from yaml.loader import SafeLoader
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    if username == 'jsmith':
+        st.write(f'Welcome *{name}*')
+        st.title('Application 1')
+    elif username == 'rbriggs':
+        st.write(f'Welcome *{name}*')
+        st.title('Application 2')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
